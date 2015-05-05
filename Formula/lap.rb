b4f911fd517d83f4d4b748a97b96a7aa3402c355
ivyduk <ivanduquecs@gmail.com>
2015-05-05 04:07:49 -0500

    New Formulas without complete functional code added to the folder

---
+++ Formula/lap.rb
@@ -0,0 +1,12 @@
+class Lap < Formula
+  url "http://www.cbcb.umd.edu/~cmhill/files/lap_release_1.1.zip"
+  version "1.1"
+  homepage "http://assembly-eval.sourceforge.net/"
+
+
+  # depends_on "cmake" => :build
+
+  def install
+    #python executable files
+  end
+end
